9d502b79c110016d7dec8076a61baa7fd58a595a
Ashish auti <ashishauti123@gmail.com>
2026-07-06 05:54:50 +0000

    feat : use pypdf to read pdf doc

--- pdf-chatbot/app.py
+++ pdf-chatbot/app.py
@@ -1,4 +1,5 @@
 import streamlit as st 
+from pypdf import PdfReader
 
 st.set_page_config(
     page_title="pdf chatbot",
@@ -10,4 +11,20 @@ st.title("📄 chat with your PDF")
 uploaded_file = st.file_uploader('upload a pdf', type=['pdf'])
 
 if uploaded_file:
-    st.success(f'uploaded : {uploaded_file.name}')
+    reader = PdfReader(uploaded_file)
+    text = ''
+
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
+
+    st.success('pdf loaded successfully')
+
+    st.subheader('Extracted Text')
+
+    st.text_area(
+        'content ',
+        text,
+        height=300
+    )
+
+    # st.success(f'uploaded : {uploaded_file.name}')
